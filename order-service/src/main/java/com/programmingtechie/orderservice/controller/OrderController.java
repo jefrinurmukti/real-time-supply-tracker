2a898a1e5906e34b3a81734065a47bcde432a9dd
SohaibKhan007 <sohaibkhanatwork@gmail.com>
2025-05-28 12:12:57 +0530

    build the order-service

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.programmingtechie.orderservice.controller;
+
+import com.programmingtechie.orderservice.dto.OrderRequest;
+import com.programmingtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    // name is same as in application properties file
+    /*@CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="inventory")
+    @Retry(name = "inventory")*/
+    // completeablefuture as it is an asynchronous call
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        return orderService.placeOrder(orderRequest);
+
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please try again later!");
+    }
+}
